7e0bdb6dbe6b2547ff23d4f0f95fee74dc7338df
gzahn <geoff.gone@gmail.com>
2018-10-24 10:15:27 -0600

    v1.5

--- SEPHAT_code.R
+++ SEPHAT_code.R
@@ -1,9 +1,11 @@
 library(ggplot2)
 dat = read.csv("./SEPHAT.csv")
 
+# quickly plot relative abundance
 ggplot(dat, aes(x=Week, y= RelAbund, color=Location, group = Location)) +
   geom_point() + stat_smooth(method = "lm") 
 
+# Build a statistical model of relative abundance, explained by location and week
 model = aov(RelAbund ~ Location * factor(Week), data = dat)
 summary(model)
 TukeyHSD(model)$Location
