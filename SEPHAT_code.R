b98e5e5b9c0671706099e16489ac8ea962a185b0
gzahn <geoff.gone@gmail.com>
2018-10-24 10:23:17 -0600

    v1.5

--- SEPHAT_code.R
+++ SEPHAT_code.R
@@ -2,11 +2,19 @@ library(ggplot2)
 dat = read.csv("./SEPHAT.csv")
 
 # quickly plot relative abundance
-ggplot(dat, aes(x=Week, y= RelAbund, color=Location, group = Location)) +
-  geom_point() + stat_smooth(method = "lm") 
+p = ggplot(dat, aes(x=Week, y= RelAbund, color=Location, group = Location)) +
+  geom_point() + stat_smooth() 
+p
+
+# Can make the plot nicer, if we want
+p + theme_minimal() +
+  scale_color_brewer(labels = c("Hatchery","Sewage Plant"),type = "qual", palette = 2) +
+  labs(y="Relative Abundance of Coliform Bacteria")
 
 # Build a statistical model of relative abundance, explained by location and week
 model = aov(RelAbund ~ Location * factor(Week), data = dat)
 summary(model)
 TukeyHSD(model)$Location
+
+#### None of the above works unless data is TIDY ####
 
